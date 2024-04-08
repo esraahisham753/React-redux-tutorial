d8ec3c11c20bf93ad00c24031554e1741e5c2085
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-08 11:49:57 +0000

    Add booklist and reset styles

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Greeting = () => {
-  return <h2>That is my first component</h2>;
-};
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>  
+  )  
+}
+
+const Book = () => {
+  return (
+   <article>
+     <Image />
+     <Title />
+     <Author />
+   </article> 
+  )
+}
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/913C+MR3S5L._SY425_.jpg" alt="The Women: A Novel" />
+const Title = () => <h2>The Women: A Novel </h2>
+const Author = () => <h4>Kristin Hannah</h4>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
