5d46fe8413a765da3dda4dd38168b04b6e84314b
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-09 12:04:03 +0000

    Add dynamic booklist

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,23 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const books = [
+  {
+  author: "Kristin Hannah",
+  title: "The Women: A Novel",
+  img: "./images/book-1.jpg"
+  },
+  {
+  author: 'Jonathan Haidt',
+  title: 'The Anxious Generation',
+  img: 'https://m.media-amazon.com/images/I/81XP4hEXDXL._SY425_.jpg'
+  }
+
+]
+
+
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+
+console.log(newNames);
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+     {names.map((name) => {
+        return <h1>{name}</h1>
+    })}
     </section>  
   )  
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { author, title, img} = props;
+  console.log(props);
+
   return (
    <article className="book">
-     <img src="./images/book-1.jpg" alt="The Women: A Novel" />
-     <h2>The Women: A Novel </h2>
-     <h4>Kristin Hannah</h4>
+     <img src={img} alt={title} />
+     <h2>{title}</h2>
+     <h4>{author}</h4>
    </article> 
   )
 }
