4860db85da166b9a8a79387cf3fa7437df8f0691
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-13 18:00:47 +0000

    Refactor book component and data

---
+++ tutorial/src/books.js
@@ -0,0 +1,16 @@
+export const books = [
+    {
+      author: "Kristin Hannah",
+      title: "The Women: A Novel",
+      img: "./images/book-1.jpg",
+      id: 1,
+    },
+    {
+      author: "Jonathan Haidt",
+      title: "The Anxious Generation",
+      img: "https://m.media-amazon.com/images/I/81XP4hEXDXL._SY425_.jpg",
+      id: 2,
+    },
+  ];
+
+  
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,21 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-const books = [
-  {
-    author: "Kristin Hannah",
-    title: "The Women: A Novel",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    author: "Jonathan Haidt",
-    title: "The Anxious Generation",
-    img: "https://m.media-amazon.com/images/I/81XP4hEXDXL._SY425_.jpg",
-    id: 2,
-  },
-];
+import {books} from "./books";
 
 const BookList = () => {
   return (
@@ -26,19 +12,6 @@ const BookList = () => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-
-const Book = (props) => {
-  const { author, title, img } = props;
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
