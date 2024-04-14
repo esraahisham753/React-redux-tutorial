ae57c95a31638c6e40f3b04b138f16471b282be9
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-14 18:00:44 +0000

    Add use state array state and markup

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import { data } from "../../../data";
 
+const Record = (props) => {
+  const { id, name } = props;
+
+  return (
+    <li>
+      {name} <button type="button">Remove</button>
+    </li>
+  );
+};
+
 const UseStateArray = () => {
-  const [data, setData] = useState(data);
+  const [records, setRecords] = useState(data);
 
   return (
     <section className="data">
-      <ul className="data__list"></ul>
+      <ul className="data__list">
+        {records.map((record) => {
+          return <Record {...record} key={record.id}/>
+        })}
+      </ul>
+      <button type="button">Clear List</button>
     </section>
   );
 };
