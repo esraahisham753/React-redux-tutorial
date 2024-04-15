c65dabd82dd3b6b7f2c369886368b19fc77a7012
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-15 16:09:31 +0000

    Fetch github users

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
   const [users, setUsers] = useState([]);
@@ -8,21 +8,47 @@ const FetchData = () => {
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(url);
-      if (! response.ok) {
+      if (!response.ok) {
         throw Error("The network response wasn't ok");
       }
       const jsonFormat = await response.json();
-      return jsonFormat;   
-    }
+      return jsonFormat;
+    };
 
-    fetchData().then((users) => {
-      console.log(users);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    fetchData()
+      .then((users) => {
+        setUsers(users);
+        console.log(users);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
-  return <h2>fetch data example</h2>;
+  return (
+    <section className="cards">
+      <h1 className="cards__title">Github users</h1>
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        console.log("name: ", login);
+        return (
+          <div className="card" key={id}>
+            <img
+              src={avatar_url}
+              alt="Profile photo"
+              width={100}
+              height={100}
+            />
+            <div className="card__info">
+              <h2 className="card__name">{login}</h2>
+              <a href={html_url} className="card__link" target="_blank">
+                Profile link
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 export default FetchData;
