355f36ba17c4805c6c6deb8b5c2a4ac2011903aa
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-15 18:01:00 +0000

    Complete conditional rendering basics

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,42 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+
+      if (! response.ok) {
+        throw Error("Network request error");
+      }
+
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    };
+
+    fetchData().then((user) => {
+      console.log(user);
+      setUser(user);
+      setIsLoading(false);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  if(isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  return (
+    <section className="user">
+      <h2 className='user__name'>{user.login}</h2>
+      <img src={user.avatar_url} alt={user.login} className="user__avatar" />
+      <p className="user_bio">{user.bio}</p>
+      <p className="user__company">Works at: {user.company}</p>
+    </section>
+  );
 };
 export default MultipleReturnsFetchData;
