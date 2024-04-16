6f145a71570e2ee674fbac7f52a7da958158f7c7
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-16 18:00:44 +0000

    Show initial render when mounting components back

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [visible, setVisible] = useState(true);
+
+  const handleButtonClick = () => {
+    setVisible((currentState) => !currentState);
+  };
+
+  return (
+    <section>
+      <button className="btn" onClick={handleButtonClick}>Toggle</button>
+      {visible && <Component />}
+    </section>
+  );
 };
+
+const Component = () => {
+  useEffect(() => {
+    console.log('Hi from the second component');
+  }, []);
+
+  return <h2>Second Component</h2>;
+}
 
 export default CleanupFunction;
