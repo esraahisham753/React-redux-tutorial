9946e847490d7310a4a8943fb06ddfb01f9b1122
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-16 11:33:36 +0000

    Complete fetch and short circuit examples

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,42 +1,57 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(url);
+      try {
+        const resp = await fetch(url);
+        if (!resp.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+        const user = await resp.json();
 
-      if (! response.ok) {
-        throw Error("Network request error");
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
-
-      const jsonResponse = await response.json();
-      return jsonResponse;
     };
 
-    fetchData().then((user) => {
-      console.log(user);
-      setUser(user);
-      setIsLoading(false);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    fetchData();
   }, []);
 
-  if(isLoading) {
-    return <h2>Loading...</h2>
+  if (isLoading) {
+    return <h2>Loading...</h2>;
   }
+
+  if (isError) {
+    return <h2>There is an Error</h2>;
+  }
+
+  const { avatar_url, login, bio, company } = user;
 
   return (
     <section className="user">
-      <h2 className='user__name'>{user.login}</h2>
-      <img src={user.avatar_url} alt={user.login} className="user__avatar" />
-      <p className="user_bio">{user.bio}</p>
-      <p className="user__company">Works at: {user.company}</p>
+      <h1>Fetch Data</h1>
+      <img
+        src={avatar_url}
+        alt={login}
+        className="user__avatar"
+        width={200}
+        height={200}
+      />
+      <h2 className="user__name">{login}</h2>
+      <p className="user__company">Works at: {company}</p>
+      <p className="user__bio">{bio}</p>
     </section>
   );
 };
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,19 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [name, setName] = useState('esraa');
+  const [number, setNumber] = useState(0);
+
+  const codeExample = number || 'Hello World';
+
+  return (
+      <section>
+        <h4>Falsy OR: {number || 'hello world'}</h4>
+        <h4>Falsy AND: {number && 'hello world'}</h4>
+        <h4>Truthy OR: {name || 'hello world'}</h4>
+        <h4>Truthy AND: {name && 'hello world'}</h4>
+        {codeExample}
+      </section>
+  );
 };
 export default ShortCircuitOverview;
