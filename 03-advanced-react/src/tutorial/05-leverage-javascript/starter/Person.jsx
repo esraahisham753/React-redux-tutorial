97c0ddf2c7a5656b3219dc0173d3b515f6868fa7
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-17 17:39:23 +0000

    Add default parameter for nickname

--- 03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ 03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,10 +1,10 @@
-export function Person({ name, nickName, images }) {
+export function Person({ name, nickName = 'ShakeAndBake', images }) {
   console.log(name);
   return (
     <div>
       <h4>{name}</h4>
-      <h4>{nickName}</h4>
-      <img src={images[0].small.url} alt={name} />
+      <p>Nickname: {nickName}</p>
+      {/*<img src={images[0].small.url} alt={name} />*/}
     </div>
   );
 }
