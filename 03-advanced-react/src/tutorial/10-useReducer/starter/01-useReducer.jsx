d395649ed8e85a9b1885c9a01b3f16ab7ba42d27
Esraa Abduallah <esraahisham753@gmail.com>
2024-04-25 12:42:57 +0000

    Complete useReducer clear items action

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
-import { data } from '../../../data';
-const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+import React, { useReducer } from "react";
+import { data, people } from "../../../data";
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_LIST") {
+    return { ...state, people: [] };
+  }
+};
+
+const ReducerBasics = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const removeItem = (id) => {};
+
+  const resetItems = () => {};
+
+  const clearList = () => dispatch({ type: "CLEAR_LIST" });
+
+  console.log(state);
+
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {state.people.length ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}>
+          clear items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetItems}>
+          reset items
+        </button>
+      )}
     </div>
   );
 };
